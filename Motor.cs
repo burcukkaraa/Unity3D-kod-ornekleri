42cd8588d5b72f213674b40999eff5458c22ccd9
burcukkaraa <37186307+burcukkaraa@users.noreply.github.com>
2018-03-08 20:16:36 +0300

    Add files via upload

---
+++ Motor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour {
+
+
+	public GameObject oyuncu;
+	public GameObject kure;
+	public int adet = 50;
+
+    
+
+	public Material [] materyal ;
+	int sayi = -1 ;
+	int denkle ;
+
+
+
+	void Start () {
+
+        
+
+		for (int i = 0; i < adet; i++) 
+		{
+
+			sayi++;
+			sayi = sayi % materyal.Length;
+			kure.GetComponent <Renderer> ().material = materyal [sayi];
+
+
+
+
+			Vector3 position = new Vector3 (Random.Range (-8.0F, 8.0F), 3, Random.Range (-8.0F, 8.0F));
+			Instantiate (kure, position, Quaternion.identity);
+
+
+			kure.name = "T" + materyal [sayi];
+
+
+			if (sayi > 3) {
+				kure.name = "T" + materyal [0];
+			} else if (sayi <= 3) {
+
+				kure.name = "T" + materyal [sayi +1];
+			}
+		}
+		
+	}
+	
+
+}
---
+++ accelerometer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using System.IO;
+using System.Text;
+//using Excel= Microsoft.Office.Interop.Excel;
+//using Microsoft.Office.Interop.Excel;
+
+
+
+
+public class accelerometer : MonoBehaviour {
+
+    Vector3 ilkPozisyon;
+    public Rigidbody rb;
+
+
+    void Start()
+    {
+        ilkPozisyon = Input.acceleration;
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void FixedUpdate()
+    {
+		rb.AddForce(new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical")) * 10);
+		rb.AddForce(new Vector3(Input.acceleration.x - ilkPozisyon.x,0, -Input.acceleration.y + ilkPozisyon.y) * 30);
+    }
+
+	
+	
+	
+       
+}
